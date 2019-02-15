a8551f9152e9ca9b9324ddc10674d7d7b79de20b
mikkel <mikkel465@live.dk>
2019-02-15 11:08:19 +0100

    updated for easier ML

--- Analysis/analysis_tools.py
+++ Analysis/analysis_tools.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import interpolate
+import sys
 
 def spline_data(time_array,data_array):
 
@@ -9,14 +10,80 @@ def spline_data(time_array,data_array):
 	# arange data in chronological order
 	time = np.array(time_array)
 	data = np.array(data_array)
+	time = np.append(time, [15000.])
+	data = np.append(data, [data[-1]])
+
 
 	#tck = interpolate.splrep(time, data)
 	#spline_vals = interpolate.splev(time_new, tck)
 	tck = interpolate.PchipInterpolator(time, data)
 	spline_vals = tck(time_new)
 
-	#print(time_new, spline_vals)
 	return time_new, spline_vals
+
+def step_spline(time_array,data_array):
+
+	# values where the spline is evaluated
+	time_new = np.linspace(0,10000,21)
+	
+	# arange data in chronological order
+	time = np.array(time_array)
+	data = np.array(data_array)
+
+	run = True
+	i_d = 1
+	while run == True:
+		
+		try: a = data[i_d]
+		except:
+			run = False
+			continue
+
+		if data[i_d - 1] != data[i_d]:
+			data = np.insert(data, i_d, data[i_d - 1])
+			time = np.insert(time, i_d, time[i_d] - 0.1)
+			i_d += 1
+		
+		i_d += 1
+
+	time = np.append(time, [15000.])
+	data = np.append(data, [data[-1]])
+
+	tck = interpolate.PchipInterpolator(time, data)
+	spline_vals = tck(time_new)
+
+	#import matplotlib.pyplot as plt
+	#plt.plot(time_new,spline_vals)
+	#plt.plot(time,data)
+	#plt.show()
+
+	return time_new, spline_vals
+
+def next_goal_spline(goal_times_home,goal_times_away):
+
+	# values where the spline is evaluated
+	time_new = np.linspace(0,10000,21)
+	next_goal_data = []
+	
+	# arange data in chronological order
+	goals_home = np.append(np.array(goal_times_home),15000.)
+	goals_away = np.append(np.array(goal_times_away),15000.)
+
+	for i_t in range(0,len(time_new)):
+		next_home = max([i for i in (time_new[i_t] - goals_home) if i < 0])
+		next_away = max([i for i in (time_new[i_t] - goals_away) if i < 0])
+
+		if next_home > next_away:
+			next_goal_data.append(0)
+		elif next_home == next_away and next_home == min(time_new[i_t] - goals_home):
+			next_goal_data.append(1)
+		elif next_home < next_away:
+			next_goal_data.append(2)
+		else:
+			next_goal_data.append(np.inf)
+	
+	return time_new, next_goal_data
+
 
 def check_match_data(match_data):
 
